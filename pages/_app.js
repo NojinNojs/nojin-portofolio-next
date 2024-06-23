304197ac608e4718d7fed90fa1354a1d8600aeeb
NojsNojin <raffiaksan3108@gmail.com>
2024-06-23 20:03:04 +0700

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,6 @@
 import "../styles/globals.css";
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  <Head>
-    <title>Nojin's Programming Portfolio</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-  </Head>;
   return <Component {...pageProps} />;
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -6,16 +6,20 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta name="description" content="Nojin's programming portfolio. Discover projects, skills, and contact information for Raffi Aqsan, a dedicated programmer." />
-          <meta name="keywords" content="Nojin programming, Nojin portfolio, Raffi Aqsan, programming, web development" />
+          <meta name="keywords" content="Nojin programming, Nojin portfolio, Raffi Aqsan, programming, web development, nojin site, nojin, nojs, nojsnojin, nojin.site, nojin.site, muhammad raffi aqsan, raffi dosq 15, website raffi, website nojin, portofolio nojin, portofolio raffi, Nojs Nojin, Photography, Portofolio, website" />
           <meta property="og:title" content="Nojin's Programming Portfolio" />
           <meta property="og:description" content="Explore the programming projects and skills of Raffi Aqsan. Connect and collaborate with Nojin on various programming endeavors." />
-          <meta property="og:image" content="/seo-banner.jpg" />
+          <meta property="og:image" content="/banner-nojin-website.webp" />
           <meta property="og:url" content="https://nojin.site" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Nojin's Programming Portfolio" />
           <meta name="twitter:description" content="Explore the programming projects and skills of Raffi Aqsan. Connect and collaborate with Nojin on various programming endeavors." />
-          <meta name="twitter:image" content="/seo-banner.jpg" />
+          <meta name="twitter:image" content="/banner-nojin-website.webp" />
         </Head>
         <body>
           <Main />
